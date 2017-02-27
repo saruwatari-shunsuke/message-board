f74742adfb5bd62fe4435e5e60e25c09afec49ab
Shunsuke Saruwatari <ssaruwatari@agentgate.jp>
2017-02-27 11:05:01 +0000

    edit&destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'messages#index'
-  resources :messages , only: [:create]
-#  resources :messages, except: [:index, :new]
+  resources :messages, except: [:index, :new]
+#  resources :messages , only: [:create]
 end
